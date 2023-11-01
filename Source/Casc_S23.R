308f14d94c9aeda6bf2e915d2a641e35bb68ec4c
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-01 09:18:26 +0100

    Create Casc_S23.R

---
+++ Source/Casc_S23.R
@@ -0,0 +1,85 @@
+
+######
+#casc
+######
+cascSpace <- st_read("cascapedia.KML") %>%
+  st_transform(32620)
+
+cascSpace <- cascSpace %>%
+  slice(1) %>%
+  st_difference(cascSpace %>% slice(2)) %>%
+  st_difference(cascSpace %>% slice(3)) %>%
+  st_difference(cascSpace %>% slice(4))
+
+urm <- 32620
+
+casc_selected <- dplyr::select(cascSpace, geometry) %>% st_zm()
+
+
+
+##########
+#cascplot
+##########
+
+casc.df <- subset(gaspe.df, lake=="Cascapedia")
+cascS23.df <- subset(casc.df, Season=="S23")
+cascNet.df <- subset(cascS23.df, type=="NET")
+
+
+##############
+#convert nets
+##############
+
+cascNet_tibble.df <- as_tibble(cascNet.df)
+cascNet_tibble.df <- dplyr::filter(cascNet_tibble.df, !is.na(lonDD) & !is.na(latDD))
+cascNet_space.df <- st_as_sf(cascNet_tibble.df, coords = c("lonDD", "latDD"))
+cascNet_space.df <- st_set_crs(cascNet_space.df, 4326)
+
+
+# Select the 'geometry' column from 'th' and set Z and M values
+casc_selected <- dplyr::select(cascSpace, geometry) %>% st_zm()
+
+cascS23_plot <- ggplot() +
+  geom_sf(data = casc_selected, color="#343A40", fill="#ADB5BD") + 
+  geom_sf(data = cascNet_space.df, aes(color = "cascNet_space", shape = "cascNet_space"), show.legend = FALSE) +
+  #geom_path(data = ThibaultNet_space.df, aes(x = your_x_column, y = your_y_column, group = group_column), color = "blue") +  # Replace your_x_column, your_y_column, and group_column with appropriate column names
+  theme(panel.grid = element_blank(),
+        axis.text.x= element_blank(),
+        axis.text.y= element_blank(),
+        panel.background = element_rect(fill = "transparent", color = NA), 
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.key = element_rect(fill = "transparent"), 
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) +
+  #legend.text = element_text(size=8), 
+  #legend.position = c(0.05, .95), 
+  #legend.justification = c("right", "bottom"))
+  scale_color_manual(name = "Legend", 
+                     values = c ("black"),
+                     labels = c("Nets")) +
+  scale_fill_manual(name = "Legend", 
+                    values = c( "black" ),
+                    labels = c("Nets")) +
+  scale_shape_manual(name = "Legend", 
+                     values = c(16),
+                     labels = c("Nets"))
+
+# Add scale and North arrow
+cascS23_plot <- cascS23_plot+
+  ggspatial::annotation_scale(
+    location = "br",
+    bar_cols = c("grey60", "white"),
+    text_family = "ArcherPro Book"
+  ) +
+  ggspatial::annotation_north_arrow(
+    location = "tl", which_north = "true",
+    pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
+    style = ggspatial::north_arrow_fancy_orienteering(
+      fill = c("grey40", "white"),
+      line_col = "grey20",
+      text_family = "ArcherPro Book"
+    )
+  )
+
+
+print(cascS23_plot)
