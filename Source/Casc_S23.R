958a51e1915186140ad7e4d22f5be47326b0d840
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-06 12:51:37 +0100

    Add lines

--- Source/Casc_S23.R
+++ Source/Casc_S23.R
@@ -35,6 +35,25 @@ cascNet_tibble.df <- dplyr::filter(cascNet_tibble.df, !is.na(lonDD) & !is.na(lat
 cascNet_space.df <- st_as_sf(cascNet_tibble.df, coords = c("lonDD", "latDD"))
 cascNet_space.df <- st_set_crs(cascNet_space.df, 4326)
 
+connections_df <- data.frame(
+  from = c(1, 2, 3),  # Index of the starting points in sf_object
+  to = c(4, 5, 6)     # Index of the ending points in sf_object
+  
+)
+
+
+line <- st_sfc(st_linestring(st_coordinates(cascNet_space.df)),
+               crs = st_crs(cascNet_space.df))
+
+allCoords <- as.matrix(st_coordinates(cascNet_space.df))
+lines <- lapply(1:nrow(connections_df),
+                function(r){
+                  rbind(allCoords[connections_df[r,1], ],
+                        allCoords[connections_df[r,2], ])
+                }) %>%
+  st_multilinestring(.) %>%
+  st_sfc(., crs = st_crs(cascNet_space.df))
+
 
 # Select the 'geometry' column from 'th' and set Z and M values
 casc_selected <- dplyr::select(cascSpace, geometry) %>% st_zm()
@@ -42,7 +61,7 @@ casc_selected <- dplyr::select(cascSpace, geometry) %>% st_zm()
 cascS23_plot <- ggplot() +
   geom_sf(data = casc_selected, color="#343A40", fill="#ADB5BD") + 
   geom_sf(data = cascNet_space.df, aes(color = "cascNet_space", shape = "cascNet_space"), show.legend = FALSE) +
-  #geom_path(data = ThibaultNet_space.df, aes(x = your_x_column, y = your_y_column, group = group_column), color = "blue") +  # Replace your_x_column, your_y_column, and group_column with appropriate column names
+  geom_sf(data = lines, color = "black", linetype="dashed") +
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
