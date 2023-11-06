534b3151ac1ea203c08e572d8df7026c5adec96b
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-06 12:51:45 +0100

    Add lines

--- Source/Gouache_S22.R
+++ Source/Gouache_S22.R
@@ -23,7 +23,7 @@ gouche_selected <- dplyr::select(goucheSpace, geometry) %>% st_zm()
 gouche.df <- subset(gaspe.df, lake=="Gouache")
 goucheS22.df <- subset(gouche.df, Season=="S22")
 goucheNet.df <- subset(goucheS22.df , type=="NET")
-
+gouchetrap.df <- subset(goucheS22.df , type=="Trap")
 
 ##############
 #convert nets
@@ -34,35 +34,61 @@ goucheNet_tibble.df <- dplyr::filter(goucheNet_tibble.df, !is.na(lonDD) & !is.na
 goucheNet_space.df <- st_as_sf(goucheNet_tibble.df, coords = c("lonDD", "latDD"))
 gouchNet_space.df <- st_set_crs(goucheNet_space.df, 4326)
 
+#########
+#Trap
+########
+gouchetrapS22_tibble.df <- as_tibble(gouchetrap.df)
+gouchetrapS22_tibble.df <- dplyr::filter(gouchetrapS22_tibble.df, !is.na(lonDD) & !is.na(latDD))
+gouchetrapS22_space.df <- st_as_sf(gouchetrapS22_tibble.df, coords = c("lonDD", "latDD"))
+gouchetrapS22_space.df <- st_set_crs(gouchetrapS22_space.df, 4326)
 
-gouache_plot <- ggplot() +
+##########
+#lines
+###########
+connections_df <- data.frame(
+  from = c(1, 2, 3, 4, 5, 6, 7, 8),  # Index of the starting points in sf_object
+  to = c(9, 10, 11, 12, 13, 14, 15, 16)     # Index of the ending points in sf_object
+  
+)
+
+
+line <- st_sfc(st_linestring(st_coordinates(gouchNet_space.df)),
+               crs = st_crs(gouchNet_space.df))
+
+allCoords <- as.matrix(st_coordinates(gouchNet_space.df))
+lines <- lapply(1:nrow(connections_df),
+                function(r){
+                  rbind(allCoords[connections_df[r,1], ],
+                        allCoords[connections_df[r,2], ])
+                }) %>%
+  st_multilinestring(.) %>%
+  st_sfc(., crs = st_crs(gouchNet_space.df))
+
+
+gouacheS22_plot <- ggplot() +
   geom_sf(data = gouche_selected, color="#343A40", fill="#ADB5BD") +
-  geom_sf(data = gouchNet_space.df, aes(color = "gouchNet_space", shape = "gouchNet_space"), show.legend = FALSE) +
+  geom_sf(data = gouchetrapS22_space.df, aes(color = "gouchetrapS22_space", shape = "gouchetrapS22_space"), show.legend = TRUE) +
+  geom_sf(data = gouchNet_space.df, aes(color = "gouchNet_space", shape = "gouchNet_space"), show.legend = TRUE) +
+  geom_sf(data = lines, color = "black", linetype="dashed") +
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
         panel.background = element_rect(fill = "transparent", color = NA), 
         axis.ticks.x = element_blank(),
-        #panel.border = element_rect(color = "black", 
-        #fill = NA, 
-        #linewidth = 2),
         axis.ticks.y = element_blank(), 
         legend.key = element_rect(fill = "transparent"), 
         plot.margin = unit(c(1,1,1,1), "cm"))+
-  #legend.text = element_text(size=8), 
-  #legend.position = c(0.05, .95), 
-  #legend.justification = c("right", "bottom"))
   scale_color_manual(name = "Legend", 
-                     values = c ("black"),
-                     labels = c("Nets")) +
+                     values = c ("#6C757D","#212529"),
+                     labels = c("Nets","Tip-Ups")) +
   scale_fill_manual(name = "Legend", 
-                    values = c( "black" ),
-                    labels = c("Nets")) +
+                    values = c( "#6C757D","#212529"),
+                    labels = c("Nets","Tip-Ups")) +
   scale_shape_manual(name = "Legend", 
-                     values = c(16),
-                     labels = c("Nets"))
+                     values = c(17, 16),
+                     labels = c("Nets","Tip-Ups"))
 
-gouache_plot <- gouache_plot+
+gouacheS22_plot <- gouacheS22_plot+
   ggspatial::annotation_scale(
     location = "br",
     bar_cols = c("grey60", "white"),
@@ -78,4 +104,4 @@ gouache_plot <- gouache_plot+
     )
   )
 
-print(gouache_plot)
+print(gouacheS22_plot)
