f2987e93bc014fefef04ca5489d5ba3d62aae988
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-07 17:28:11 +0100

    Change to solid line

--- Source/Casc_S23.R
+++ Source/Casc_S23.R
@@ -60,8 +60,8 @@ casc_selected <- dplyr::select(cascSpace, geometry) %>% st_zm()
 
 cascS23_plot <- ggplot() +
   geom_sf(data = casc_selected, color="#343A40", fill="#ADB5BD") + 
-  geom_sf(data = cascNet_space.df, aes(color = "cascNet_space", shape = "cascNet_space"), show.legend = FALSE) +
-  geom_sf(data = lines, color = "black", linetype="dashed") +
+  geom_sf(data = cascNet_space.df, aes(color = "cascNet_space", shape = "cascNet_space"), show.legend = TRUE) +
+  geom_sf(data = lines, color = "black", linetype="solid") +
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
