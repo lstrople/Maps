448ddeed7c68c97f3fc1f7537d575b243134c6d3
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-07 08:28:23 +0100

    Add lines and save

--- Source/Paul_S23.R
+++ Source/Paul_S23.R
@@ -29,6 +29,27 @@ PaulNet_tibble.df <- dplyr::filter(PaulNet_tibble.df, !is.na(lonDD) & !is.na(lat
 PaulNet_space.df <- st_as_sf(PaulNet_tibble.df, coords = c("lonDD", "latDD"))
 PaulNet_space.df <- st_set_crs(PaulNet_space.df, 4326)
 
+#######
+#Lines
+#######
+connections_df <- data.frame(
+  from = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 31, 32, 33),  # Index of the starting points in sf_object
+  to = c(16, 17, 18, 19, 20,21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 34, 35, 36)     # Index of the ending points in sf_object
+  
+)
+
+
+line <- st_sfc(st_linestring(st_coordinates(PaulNet_space.df)),
+               crs = st_crs(PaulNet_space.df))
+
+allCoords <- as.matrix(st_coordinates(PaulNet_space.df))
+lines <- lapply(1:nrow(connections_df),
+                function(r){
+                  rbind(allCoords[connections_df[r,1], ],
+                        allCoords[connections_df[r,2], ])
+                }) %>%
+  st_multilinestring(.) %>%
+  st_sfc(., crs = st_crs(PaulNet_space.df))
 
 ##########
 #Paulplot
@@ -40,6 +61,7 @@ paul_selected <- dplyr::select(paulSpace, geometry) %>% st_zm()
 paulS23_plot <- ggplot() +
   geom_sf(data = paul_selected, color="#343A40", fill="#ADB5BD") +
   geom_sf(data = PaulNet_space.df, aes(color = "PaulNet_space", shape = "PaulNet_space"), show.legend = FALSE) +
+  geom_sf(data = lines, color = "black", linetype="dashed")+
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
@@ -48,9 +70,6 @@ paulS23_plot <- ggplot() +
         axis.ticks.y = element_blank(), 
         legend.key = element_rect(fill = "transparent"), 
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) +
-  #legend.text = element_text(size=8), 
-  #legend.position = c(0.05, .95), 
-  #legend.justification = c("right", "bottom")
   scale_color_manual(name = "Legend", 
                      values = c ("black"),
                      labels = c("Nets")) +
