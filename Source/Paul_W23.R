dd8797efe32e25c65b6d64a016567aa7e3778efa
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-07 08:12:47 +0100

    Add lines

--- Source/Paul_W23.R
+++ Source/Paul_W23.R
@@ -41,6 +41,29 @@ PaulNet_tibble.df <- dplyr::filter(PaulNet_tibble.df, !is.na(lonDD) & !is.na(lat
 PaulNet_space.df <- st_as_sf(PaulNet_tibble.df, coords = c("lonDD", "latDD"))
 PaulNet_space.df <- st_set_crs(PaulNet_space.df, 4326)
 
+######
+#lines
+######
+
+connections_df <- data.frame(
+  from = c(1, 2, 3, 4, 5),  # Index of the starting points in sf_object
+  to = c(6, 7, 8, 9, 10)     # Index of the ending points in sf_object
+  
+)
+
+
+line <- st_sfc(st_linestring(st_coordinates(cascNet_space.df)),
+               crs = st_crs(PaulNet_space.df))
+
+allCoords <- as.matrix(st_coordinates(PaulNet_space.df))
+lines <- lapply(1:nrow(connections_df),
+                function(r){
+                  rbind(allCoords[connections_df[r,1], ],
+                        allCoords[connections_df[r,2], ])
+                }) %>%
+  st_multilinestring(.) %>%
+  st_sfc(., crs = st_crs(PaulNet_space.df))
+
 
 
 
@@ -51,24 +74,19 @@ PaulNet_space.df <- st_set_crs(PaulNet_space.df, 4326)
 # Select the 'geometry' column from 'th' and set Z and M values
 paul_selected <- dplyr::select(paulSpace, geometry) %>% st_zm()
 
-paul_plot <- ggplot() +
+paulW23_plot <- ggplot() +
   geom_sf(data = paul_selected, color="#343A40", fill="#ADB5BD") +
   geom_sf(data = PaulNet_space.df, aes(color = "PaulNet_space", shape = "PaulNet_space"), show.legend = FALSE) +
   geom_sf(data = PaulTU_space.df, aes(color = "PaulTU_space", shape = "PaulTU_space"), show.legend = FALSE) +
+  geom_sf(data = lines, color = "black", linetype="dashed")+
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
         panel.background = element_rect(fill = "transparent", color = NA), 
         axis.ticks.x = element_blank(),
-        panel.border = element_rect(color = "black", 
-                                    fill = NA, 
-                                    linewidth = 2),
         axis.ticks.y = element_blank(), 
         legend.key = element_rect(fill = "transparent"), 
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) +
-  #legend.text = element_text(size=8), 
-  #legend.position = c(0.05, .95), 
-  #legend.justification = c("right", "bottom")
   scale_color_manual(name = "Legend", 
                      values = c ("#6C757D","#212529"),
                      labels = c("Nets", "Tip-Ups")) +
@@ -82,7 +100,7 @@ paul_plot <- ggplot() +
 
 # Add scale and North arrow
 
-paul_plot <- paul_plot+
+paulW23_plot <- paulW23_plot+
   ggspatial::annotation_scale(
     location = "br",
     bar_cols = c("grey60", "white"),
@@ -98,5 +116,9 @@ paul_plot <- paul_plot+
     )
   )
 
-print(paul_plot)
+print(paulW23_plot)
+
+ggsave("PaulW23.png", plot =paulW23_plot, width = 7, height = 5, units = "in", dpi = 300)
+
+
 
