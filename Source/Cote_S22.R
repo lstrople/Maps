f7dd444a43156d2bb2bd6275e1fee64d3f76e39e
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-07 13:35:38 +0100

    Make solid line

--- Source/Cote_S22.R
+++ Source/Cote_S22.R
@@ -69,9 +69,9 @@ lines <- lapply(1:nrow(connections_df),
 
 coteS22_plot <- ggplot() +
   geom_sf(data = cote_selected, color="#343A40", fill="#ADB5BD") +
-  geom_sf(data = coteNet_space.df, aes(color = "coteNet_space", shape = "coteNet_space"), show.legend = FALSE) +
-  geom_sf(data = CotetrapS22_space.df, aes(color = "CotetrapS22_space", shape = "CotetrapS22_space"), show.legend = FALSE) +
-  geom_sf(data = lines, color = "black", linetype="dashed") +
+  geom_sf(data = coteNet_space.df, aes(color = "coteNet_space", shape = "coteNet_space"), show.legend = TRUE) +
+  geom_sf(data = CotetrapS22_space.df, aes(color = "CotetrapS22_space", shape = "CotetrapS22_space"), show.legend = TRUE) +
+  geom_sf(data = lines, color = "black", linetype="solid") +
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
