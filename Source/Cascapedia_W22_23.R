7e080c2ca9c1b623d42ae7b7a374da6d2b94eb1c
Leah <55734721+lstrople@users.noreply.github.com>
2023-11-08 10:03:40 +0100

    Add lines and save

--- Source/Cascapedia_W22_23.R
+++ Source/Cascapedia_W22_23.R
@@ -1,13 +1,3 @@
-##########
-#cascplot
-##########
-
-casc.df <- subset(gaspe.df, lake=="Cascapedia")
-cascTU.df <- subset(casc.df, type=="TU")
-cascTrap.df <- subset(casc.df, type=="trap")
-cascNet.df <- subset(casc.df, type=="NET")
-
-
 ######
 #casc
 ######
@@ -24,6 +14,15 @@ urm <- 32620
 
 casc_selected <- dplyr::select(cascSpace, geometry) %>% st_zm()
 
+
+##########
+#cascplot
+##########
+
+casc.df <- subset(winter.df, lake=="Cascapedia")
+cascTU.df <- subset(casc.df, type=="TU")
+cascTrap.df <- subset(casc.df, type=="trap")
+cascNet.df <- subset(casc.df, type=="NET")
 
 
 ###########
@@ -55,41 +54,60 @@ cascTrap_tibble.df <- dplyr::filter(cascTrap_tibble.df, !is.na(lonDD) & !is.na(l
 cascTrap_space.df <- st_as_sf(cascTrap_tibble.df, coords = c("lonDD", "latDD"))
 cascTrap_space.df <- st_set_crs(cascTrap_space.df, 4326)
 
+#######
+#Lines
+######
+
+connections_df <- data.frame(
+  from = c(1, 2, 3),  # Index of the starting points in sf_object
+  to = c(5, 6, 6)     # Index of the ending points in sf_object
+  
+)
+
+
+line <- st_sfc(st_linestring(st_coordinates(cascNet_space.df)),
+               crs = st_crs(cascNet_space.df))
+
+allCoords <- as.matrix(st_coordinates(cascNet_space.df))
+lines <- lapply(1:nrow(connections_df),
+                function(r){
+                  rbind(allCoords[connections_df[r,1], ],
+                        allCoords[connections_df[r,2], ])
+                }) %>%
+  st_multilinestring(.) %>%
+  st_sfc(., crs = st_crs(cascNet_space.df))
+
+
+
 # Select the 'geometry' column from 'th' and set Z and M values
 casc_selected <- dplyr::select(cascSpace, geometry) %>% st_zm()
 
-casc_plot <- ggplot() +
-  geom_sf(data = casc_selected, color="#343A40", fill="#ADB5BD") + 
-  geom_sf(data = cascTrap_space.df, aes(color = "cascTrap_space", shape = "cascTrap_space"), show.legend = FALSE) +
-  geom_sf(data = cascTU_space.df, aes(color = "cascTU_space", shape = "cascTU_space"), show.legend = FALSE) +
-  geom_sf(data = cascNet_space.df, aes(color = "cascNet_space", shape = "cascNet_space"), show.legend = FALSE) +
-  #geom_path(data = ThibaultNet_space.df, aes(x = your_x_column, y = your_y_column, group = group_column), color = "blue") +  # Replace your_x_column, your_y_column, and group_column with appropriate column names
+cascW_plot <- ggplot() +
+  geom_sf(data = casc_selected, color="#343A40", fill="#DEE2E6") + 
+  geom_sf(data = cascTrap_space.df, aes(color = "cascTrap_space", shape = "cascTrap_space"), show.legend = TRUE) +
+  geom_sf(data = cascTU_space.df, aes(color = "cascTU_space", shape = "cascTU_space"), show.legend = TRUE) +
+  geom_sf(data = cascNet_space.df, aes(color = "cascNet_space", shape = "cascNet_space"), show.legend = TRUE) +
+  geom_sf(data = lines, color = "black", linetype="solid") +
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
         panel.background = element_rect(fill = "transparent", color = NA), 
         axis.ticks.x = element_blank(),
         axis.ticks.y = element_blank(),
-        panel.border = element_rect(color = "black", 
-                                    fill = NA, 
-                                    linewidth = 2),
         legend.key = element_rect(fill = "transparent"), 
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) +
-  #legend.text = element_text(size=8), 
-  #legend.position = c(0.05, .95), 
-  #legend.justification = c("right", "bottom"))
   scale_color_manual(name = "Legend", 
-                     values = c ("black","#6C757D","#212529"),
+                     values = c ("black","#ADB5BD","#495057"),
                      labels = c("Nets","Traps","Tip-Ups")) +
   scale_fill_manual(name = "Legend", 
-                    values = c( "black","#6C757D","#212529" ),
+                    values = c( "black","#ADB5BD","#495057" ),
                     labels = c("Nets","Traps","Tip-Ups")) +
   scale_shape_manual(name = "Legend", 
                      values = c(16,15, 17),
                      labels = c("Nets","Traps","Tip-Ups"))
 
 # Add scale and North arrow
-casc_plot <- casc_plot+
+cascW_plot <- cascW_plot+
   ggspatial::annotation_scale(
     location = "br",
     bar_cols = c("grey60", "white"),
@@ -106,6 +124,6 @@ casc_plot <- casc_plot+
   )
 
 
-print(casc_plot)
+print(cascW_plot)
 
-
+ggsave("CascW.png", plot =cascW_plot, width = 7, height = 5, units = "in", dpi = 300)
