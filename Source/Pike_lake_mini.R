3e98fa5dc1b746308fe57e6f2fb6257ba22420f9
Leah <55734721+lstrople@users.noreply.github.com>
2024-01-19 07:25:59 +0100

    Update Pike_lake_mini.R

--- Source/Pike_lake_mini.R
+++ Source/Pike_lake_mini.R
@@ -58,9 +58,9 @@ pikeANG_space.df <- st_set_crs(pikeANG_space.df, 4326)
 
 pike_plot <- ggplot() +
   geom_sf(data = pikeM_selected, color="#343A40", fill="#DEE2E6") + 
+  geom_sf(data = pikeMT_space.df, aes(color = "pikeMT_space",fill = "pikeMT_space", shape = "pikeMT_space"), show.legend = TRUE) +
   geom_sf(data = pikeHGN_space.df, aes(color = "pikeHGN_space", fill = "pikeHGN_space",  shape = "pikeHGN_space"), show.legend = TRUE) +
   geom_sf(data = pikeANG_space.df, aes(color = "pikeANG_space", fill = "pikeANG_space", shape = "pikeANG_space"), show.legend = TRUE) +
-  geom_sf(data = pikeMT_space.df, aes(color = "pikeMT_space",fill = "pikeMT_space", shape = "pikeMT_space"), show.legend = TRUE) +
   theme(panel.grid = element_blank(),
         axis.text.x= element_blank(),
         axis.text.y= element_blank(),
@@ -71,13 +71,13 @@ pike_plot <- ggplot() +
         plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm")) +
   scale_color_manual(name = "Legend", 
                      values = c ("black","black","black"),
-                     labels = c("Nets","Traps","Tip-Ups")) +
+                     labels = c("Angling","Tip-Ups", "Traps")) +
   scale_fill_manual(name = "Legend", 
-                    values = c( "black","#f8f9fa","#ADB5BD"),
-                    labels = c("Nets","Traps","Tip-Ups")) +
+                    values = c( "black","#ADB5BD","#f8f9fa"),
+                    labels = c("Angling","Tip-Ups", "Traps")) +
   scale_shape_manual(name = "Legend", 
-                     values = c(21, 22, 24),
-                     labels = c("Nets","Traps","Tip-Ups"))# Add scale and North arrow
+                     values = c(21, 24, 22),
+                     labels = c("Angling","Tip-Ups", "Traps"))# Add scale and North arrow
 pike_plot <- pike_plot+
   ggspatial::annotation_scale(
     location = "bl",
