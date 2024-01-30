cc12add0c6adf0210496237e17975ef028298ae1
Leah <55734721+lstrople@users.noreply.github.com>
2024-01-30 06:56:34 +0100

    Create Recievers_Thibault.R

---
+++ Source/Recievers_Thibault.R
@@ -0,0 +1,81 @@
+###################################
+#must run main map file before use
+###################################
+
+thibSpace <- st_read("thibault.KML") %>%
+  st_transform(32620)
+
+step1 <- thibSpace %>% dplyr::slice(1)
+step2 <- step1 %>% st_difference(thibSpace %>% dplyr::slice(2))
+step3 <- step2 %>% st_difference(thibSpace %>% dplyr::slice(3))
+step4 <- step3 %>% st_difference(thibSpace %>% dplyr::slice(4))
+
+# Check validity of geometries
+valid_geometries <- st_is_valid(thibSpace$geometry)
+invalid_geometries <- which(!valid_geometries)
+print(paste("Invalid geometries at indices: ", paste(invalid_geometries, collapse = ", ")))
+
+
+urm <- 32620
+
+Thib_selected <- dplyr::select(thibSpace, geometry) %>% st_zm()
+
+
+
+##############
+#convert nets
+##############
+
+# Convert 'gaspe' to a tibble
+recievers_tibble.df <- as_tibble(recievers.df)
+recievers_tibble.df.df <- dplyr::filter(recievers_tibble.df, !is.na(Long) & !is.na(Lat))
+recievers_space.df <- st_as_sf(recievers_tibble.df, coords = c("Long", "Lat"))
+recievers_space.df <- st_set_crs(recievers_space.df, 4326)
+
+
+
+######
+#Plot
+######
+
+
+thibtel_plot <- ggplot() +
+  geom_sf(data = Thib_selected, color="#343A40", fill="#ADB5BD") + 
+  geom_sf(data = recievers_space.df, aes(color = "recievers_space.df", shape = "recievers_space.df"), show.legend = TRUE)+
+  theme(panel.grid = element_blank(),
+        axis.text.x= element_blank(),
+        axis.text.y= element_blank(),
+        panel.background = element_rect(fill = "transparent", color = NA, size = 2), 
+        axis.ticks.x = element_blank(),
+        axis.ticks.y = element_blank(), 
+        legend.key = element_rect(fill = "transparent"), 
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))+
+  scale_color_manual(name = "Legend", 
+                     values = c ("black"),
+                     labels = c("Recievers")) +
+  scale_fill_manual(name = "Legend", 
+                    values = c( "black" ),
+                    labels = c("Recievers")) +
+  scale_shape_manual(name = "Legend", 
+                     values = c(16),
+                     labels = c("Recievers"))# Add scale and North arrow
+
+thibtel_plot <- thibtel_plot+
+  ggspatial::annotation_scale(
+    location = "br",
+    bar_cols = c("grey60", "white"),
+    text_family = "ArcherPro Book"
+  ) +
+  ggspatial::annotation_north_arrow(
+    location = "tr", which_north = "true",
+    pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
+    style = ggspatial::north_arrow_fancy_orienteering(
+      fill = c("grey40", "white"),
+      line_col = "grey20",
+      text_family = "ArcherPro Book"
+    )
+  )
+
+print(thibtel_plot)
+ggsave("ThibS23.png", plot =thibS23_plot, width = 7, height = 5, units = "in", dpi = 300)
+
